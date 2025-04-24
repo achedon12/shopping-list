bdf15e2c31cf68b067c189688e01a72a66f24586
leo deroin <l.deroin@netcourrier.com>
2025-04-24 12:02:17 +0200

    feat: github action

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
+
+// @ts-ignore
+const dirname = dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
     base: '/shopping-list/',
+    build: {
+        outDir: 'dist',
+        rollupOptions: {
+            input: {
+                main: resolve(dirname, 'index.html'),
+                list: resolve(__dirname, 'list.html'),
+            },
+        }
+    },
     plugins: [
         tailwindcss(),
     ],
