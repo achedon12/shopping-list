ee63fb714ba06b72f57ea9d50ff3b1b2d03dc638
leo deroin <l.deroin@netcourrier.com>
2025-04-24 11:34:54 +0200

    feat: github action reset

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
     e2e: {
         baseUrl: "http://localhost:5173",
-        supportFile: false, // ← désactive le fichier support par défaut
+        supportFile: false,
         specPattern: "cypress/e2e/**/*.cy.js",
     },
 });
