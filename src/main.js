9d042201c87000d4417bedd2fd37556cde870019
leo deroin <l.deroin@netcourrier.com>
2025-04-24 11:26:19 +0200

    feat: github action

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 export const fetchProducts = async () => {
     try {
-        const response = await fetch(`${import.meta.env.BASE_URL}liste_produits_quotidien.json`);
+        const response = await fetch('/liste_produits_quotidien.json');
         return await response.json();
     } catch (error) {
         console.error('Error fetching products:', error);
